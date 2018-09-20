263999e33cd3a7e28743fded64bfcead55741a14
Sher Minn Chong <sherminn.chong@gmail.com>
2018-09-20 11:00:49 -0400

    Add line art example

--- src/app/P5Instance.js
+++ src/app/P5Instance.js
@@ -1,60 +1,59 @@
 import P5 from 'p5';
 import ParametricPatterns from 'components/ParametricPatterns';
 
-const sketch = (p) => {
-  const gridX = 1;
-  const gridY = 1;
-  const canvasSizeX = window.innerWidth;
-  const canvasSizeY = window.innerHeight;
-  const cellSizeX = Math.ceil(canvasSizeY / gridX);
-  const cellSizeY = Math.ceil(canvasSizeY / gridY);
-  const patterns = [];
-  const BG_COLOR = [4, 31, 61];
-  const MULTI_COLORS = [[[41, 93, 150], [110, 195, 149]]];
-  const NUM_LAYERS = MULTI_COLORS.length;
-  const offsetX = (canvasSizeX - canvasSizeY)/2;
-  const offsetY = 0;
+// constants
+const rows = 50;
+const cols = 80;
+const rowSize = 10;
+const colSize = 10;
+const width = cols * colSize;
+const height = rows * rowSize;
 
-  p.setup = () => {
-    p.createCanvas(window.innerWidth, window.innerHeight);
-    p.reset();
-    // p.noLoop();
-    p.frameRate(60);
-    for (let n = 0; n < NUM_LAYERS; n++) {
-      for (let i = 0; i < gridX; i++) {
-        for (let j = 0; j < gridY; j++) {
-          patterns.push(new ParametricPatterns({
-            p5: p,
-            x: i * cellSizeX + offsetX,
-            y: j * cellSizeY + offsetY,
-            width: cellSizeX,
-            height: cellSizeY,
-            // seed: n,
-            color: MULTI_COLORS[n][0],
-            color2: MULTI_COLORS[n][1],
-            numLines: 100,
-            spacing: 0.04,
-            speed: 0.005,
-            amp: 1.2,
-          }));
-        }
+const sketch = (p) => {
+  function setup() {
+    p.createCanvas(width, height);
+    p.background('#fefee');
+    p.noLoop();
+  }
+
+  function array1(x, y) {
+    const ringSize = 60;
+    if (x < width/2 && p.dist(x, y, 0, 0) % (ringSize * 2) < ringSize)
+    {
+      p.text('O', x, y);
+    }
+
+    if (x > width/2 && p.dist(x, y, width, height) % (ringSize * 2) < ringSize)
+    {
+      p.text('+', x, y);
+    }
+  }
+
+  function array2(x, y) {
+    if (x > p.width / 2) {
+      p.text('-', x, y);
+    } else {
+      p.text('.', x, y);
+    }
+  }
+
+  function draw() {
+    p.textSize(12);
+    p.textFont('Courier New');
+    p.fill(0, 220);
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        const x = i * colSize;
+        const y = j * rowSize;
+
+        array1(x, y);
+        array2(x, y);
       }
     }
-  };
+  }
 
-  p.reset = () => {
-    patterns.forEach((pt) => {
-      pt.reset();
-    });
-  };
-
-  p.draw = () => {
-    p.background(...BG_COLOR);
-    patterns.forEach((pp) => {
-      pp.update();
-      pp.draw();
-    });
-  };
+  p.draw = draw;
+  p.setup = setup;
 
   p.saveImg = () => {
     p.save(`tadaaa-${Date.now()}.png`);
